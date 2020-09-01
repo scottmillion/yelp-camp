20488c42f0d97faed5589bd458dceb904ac22e2d
Scott Million <scottmillion@gmail.com>
2020-09-01 13:43:00 +0700

    Setup landing.ejs. Deleted home.ejs. Setup fake array for campgrounds.ejs

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-const testVar = "abc123";
-
 app.get("/", (req, res) => {
-  res.render("home", { testVar: testVar });
+  res.render("landing");
+});
+
+app.get("/campgrounds", (req, res) => {
+  const campgrounds = [
+    {
+      name: "Salmon Creek",
+      image:
+        "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    },
+    {
+      name: "Tallgreen Highlands",
+      image:
+        "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    },
+    {
+      name: "Green Hills",
+      image:
+        "https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
+    },
+  ];
+  res.render("campgrounds", { campgrounds: campgrounds });
 });
 
 app.listen(3000, (req, res) => {
