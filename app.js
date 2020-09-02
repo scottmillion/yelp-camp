74363070ab305e7788601491c6c0ca51d69b8d68
Scott Million <scottmillion@gmail.com>
2020-09-02 15:40:43 +0700

    Added SHOW functionality

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 // mongoose create Schema
 const campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 
 });
 
@@ -27,11 +28,20 @@ const Campground = mongoose.model('Campground', campgroundSchema);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+
+
+// Campground.create({
+//   name: "Green Hills",
+//   image: "https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
+//   description: "Who needs blue or white or yellow? We got nothin' but green here."
+// });
+
 // routes
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
+// INDEX route **RESTful** - Display a list of all campgrounds
 app.get("/campgrounds", (req, res) => {
   // Passing campgrounds array of objects from database to campgrounds.ejs view
   Campground.find({}, (err, allCampgrounds) => {
@@ -39,18 +49,20 @@ app.get("/campgrounds", (req, res) => {
       console.log("Oh no, 'find' error!");
       console.log(err);
     } else {
-      res.render("campgrounds", { campgrounds: allCampgrounds });
+      res.render("index", { campgrounds: allCampgrounds });
     }
   });
 });
 
+// CREATE route **RESTful** - Add new campground to database
 app.post("/campgrounds", (req, res) => {
   // get data from form and store it in an object
   // Note: req.body accessible b/c of body-parser
   const name = req.body.name;
   const image = req.body.image;
+  const description = req.body.description;
   // good object destructuring example. { name: name, image: image }
-  const newCampground = { name, image }
+  const newCampground = { name, image, description }
 
   // passing newCampground object to mongodb using mongoose syntax
   Campground.create(newCampground, (err, newlyCreated) => {
@@ -63,8 +75,25 @@ app.post("/campgrounds", (req, res) => {
   });
 });
 
+// NEW route **RESTful** - Displays form to make a new campground
+// NEW has to come before SHOW b/c order matters & /new triggers /:id condition
 app.get("/campgrounds/new", (req, res) => {
   res.render("new");
+});
+
+// SHOW route **RESTful** - Shows detailed info about one campground
+// SHOW has to come after NEW b/c order matters & /new triggers /:id condition
+app.get("/campgrounds/:id", (req, res) => {
+  //find the campground with provided ID
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      //render show template with that ID
+      res.render("show", { campground: foundCampground });
+    }
+  });
+
 });
 
 // remember to change this when hosted off your local machine
