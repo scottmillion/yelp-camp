aa45a1343385a1d507314c0d03b456265c2cb2c2
Scott Million <scottmillion@gmail.com>
2020-09-05 09:32:07 +0700

    Added seed data, installed faker, added comment functionality to backend and show page

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-// setup express, body-parser
-const express = require("express"),
+// APP IMPORTS
+const
+  express = require('express'),
   app = express(),
-  bodyParser = require("body-parser");
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  seedDB = require('./seeds'),
+  Campground = require('./models/campground');
+// Comment = require("./models/comment"),
+// User = require("./models/user");
 
-// mongoose setup
-const mongoose = require('mongoose');
+// DELETE DATABASE AND LOAD SEED DATA
+seedDB();
+
+// APP CONFIG
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+// MONGOOSE MONGO CONFIG
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -12,33 +24,17 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   .then(() => console.log('Connected to DB!'))
   .catch(error => console.log(error.message));
 
-// mongoose create Schema
-const campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
+// --------ROUTES--------
 
-});
-
-// mongoose give Schema and CRUD methods to Campground
-// Campground.create(), Campground.find() Campground.remove(), etc.
-const Campground = mongoose.model('Campground', campgroundSchema);
-
-// use and set
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-
-// routes
+// HOME
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
-// INDEX route **RESTful** - Display a list of all campgrounds
+// INDEX
 app.get("/campgrounds", (req, res) => {
-  // Passing campgrounds array of objects from database to campgrounds.ejs view
   Campground.find({}, (err, allCampgrounds) => {
     if (err) {
-      console.log("Oh no, 'find' error!");
       console.log(err);
     } else {
       res.render("index", { campgrounds: allCampgrounds });
@@ -46,49 +42,40 @@ app.get("/campgrounds", (req, res) => {
   });
 });
 
-// CREATE route **RESTful** - Add new campground to database
+// CREATE 
 app.post("/campgrounds", (req, res) => {
-  // get data from form and store it in an object
-  // Note: req.body accessible b/c of body-parser
   const name = req.body.name;
   const image = req.body.image;
   const description = req.body.description;
-  // good object destructuring example. { name: name, image: image, description: description }
   const newCampground = { name, image, description }
 
-  // passing newCampground object to mongodb using mongoose syntax
   Campground.create(newCampground, (err, newlyCreated) => {
     if (err) {
       console.log(err);
     } else {
-      // Redirect to campgrounds page. The default redirect is a GET request.
       res.redirect("/campgrounds");
     }
   });
 });
 
-// NEW route **RESTful** - Displays form to make a new campground
-// NEW has to come before SHOW b/c order matters & /new triggers /:id condition
+// NEW 
 app.get("/campgrounds/new", (req, res) => {
   res.render("new");
 });
 
-// SHOW route **RESTful** - Shows detailed info about one campground
-// SHOW has to come after NEW b/c order matters & /new triggers /:id condition
+// SHOW 
 app.get("/campgrounds/:id", (req, res) => {
-  //find the campground with provided ID
-  Campground.findById(req.params.id, (err, foundCampground) => {
+  Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
     if (err) {
       console.log(err);
     } else {
-      //render show template with that ID
       res.render("show", { campground: foundCampground });
     }
   });
-
 });
 
-// remember to change this when hosted off your local machine
+
+// --------SERVER--------
 app.listen(3000, (req, res) => {
   console.log("Server running on 3000, sir!");
 });
